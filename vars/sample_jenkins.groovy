38431b733544da7db49326e2f9f2e7dc493e98fa
Subramanian, Sindhuja <sindhuja.subramanian@one.verizon.com>
2017-02-26 22:57:17 -0500

    done

--- vars/sample_jenkins.groovy
+++ vars/sample_jenkins.groovy
@@ -9,6 +9,15 @@ def call(body) {
     node {
         stage ('First Stage'){
             echo "This is first stage"
+            writeFile file:'Dockerfile', text:libraryResource("Dockerfile")
+            if(fileExists('Dockerfile')){
+            echo "Dockerfile copied"
+
+       } else{
+        echo "it did not work"
+    }
+            sh "pwd"
+            sh "ls -lrt"
         }
         stage('Second Stage'){
             echo "This is second stage"
