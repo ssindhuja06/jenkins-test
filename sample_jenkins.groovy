f53851c576c94c60ea0273f40fb29f3620b08ba0
Subramanian, Sindhuja <sindhuja.subramanian@one.verizon.com>
2017-03-03 15:23:44 -0500

    testing json parse

--- sample_jenkins.groovy
+++ sample_jenkins.groovy
@@ -1,5 +1,3 @@
-import groovy.json.JsonSlurper
-
 def call(body) {
     // evaluate the body block, and collect configuration into the object
     def config = [:]
@@ -10,12 +8,16 @@ def call(body) {
     // now build, based on the configuration provided
     node {
         stage ('First Stage'){
+            echo "This is first stage"
+            writeFile file:'Dockerfile', text:libraryResource("Dockerfile")
+            if(fileExists('Dockerfile')){
+            echo "Dockerfile copied"
 
-            writeFile file:'ports.json', text:libraryResource("/ports.json")
-            def inputJson = new JsonSlurper().parseText(port.json)
-            inputJson.each{ println it }
-
-
+       } else{
+        echo "it did not work"
+    }
+            sh "pwd"
+            sh "ls -lrt"
         }
         stage('Second Stage'){
             echo "This is second stage"
